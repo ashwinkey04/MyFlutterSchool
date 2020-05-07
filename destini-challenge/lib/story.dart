4d222e0d4a62b96c5d8498bec09639f1ac87fb3c
Ashwin R <ashwinkey04@gmail.com>
2020-05-07 19:26:48 +0530

    Finish final challenge

---
+++ destini-challenge/lib/story.dart
@@ -0,0 +1,8 @@
+class Story{
+  String storyTitle, choice1, choice2;
+  Story({storyTitle,choice1,choice2}){
+   this.storyTitle=storyTitle;
+   this.choice1=choice1;
+   this.choice2=choice2;
+  }
+}
